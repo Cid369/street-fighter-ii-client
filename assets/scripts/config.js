471027a607d3b9ae0aca8d2e71911ec2d9d989e5
Cid369 <waikit.tong.82@gmail.com>
2018-12-06 17:01:57 -0500

    * Replaced proper url on production in config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'postgres://mfyphfpfvhbeja:1f359975c50f90543764711aa4e97ed0b30cb2311e94725f09f7fc33b0970c53@ec2-174-129-41-12.compute-1.amazonaws.com:5432/d9aivs3ou7sj0f',
+  production: 'https://desolate-refuge-14322.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
